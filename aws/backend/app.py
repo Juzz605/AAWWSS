324950822df017651a5c503672fc8d7edfc19888
Juzz <dhananjeyan604@gmail.com>
2026-02-01 20:55:53 +0530

    Update app.py

--- aws/backend/app.py
+++ aws/backend/app.py
@@ -21,4 +21,4 @@ def machine_state():
     return jsonify(state) if state else ("No data", 404)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000 )
