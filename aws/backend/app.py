7996b5799b86d26453c445a7fc37e8ed17215c05
Juzz <dhananjeyan604@gmail.com>
2026-02-02 20:36:14 +0530

    Update app.py

--- aws/backend/app.py
+++ aws/backend/app.py
@@ -4,7 +4,13 @@ from dotenv import load_dotenv
 import os
 import mysql.connector
 import os
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(env_path)
 
+print("DB_HOST =", os.getenv("DB_HOST"))
+print("DB_USER =", os.getenv("DB_USER"))
+print("DB_PASSWORD =", os.getenv("DB_PASSWORD"))
+print("DB_NAME =", os.getenv("DB_NAME"))
 load_dotenv()
 
 # 2. Read values from .env
