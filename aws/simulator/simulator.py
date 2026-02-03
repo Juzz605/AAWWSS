9b838f094ae317b2acda4986f5438fd7268a6891
Juzz <dhananjeyan604@gmail.com>
2026-02-03 10:50:15 +0530

    Update simulator.py

--- aws/simulator/simulator.py
+++ aws/simulator/simulator.py
@@ -3,26 +3,28 @@ import random
 import time
 
 BACKEND_URL = "http://127.0.0.1:5000/ingest"
+MACHINE_ID = "M101"
 
-MACHINES = ["M101", "M102", "M103"]
-
-def generate_sensor_data(machine_id):
+def generate_sensor_data():
     return {
-        "machine_id": machine_id,
         "temperature": round(random.uniform(40, 100), 2),
-        "vibration": round(random.uniform(1, 12), 2),
+        "vibration": round(random.uniform(1, 10), 2),
         "current": round(random.uniform(10, 50), 2),
-        "acoustic": round(random.uniform(20, 80), 2),
-        "oil_quality": round(random.uniform(20, 90), 2),
+        "acoustic": round(random.uniform(20, 90), 2),
+        "oil_quality": round(random.uniform(40, 100), 2),
         "pressure": round(random.uniform(30, 110), 2)
     }
 
 while True:
-    for machine in MACHINES:
-        payload = generate_sensor_data(machine)
-        try:
-            r = requests.post(BACKEND_URL, json=payload, timeout=2)
-            print(f"Sent data for {machine} → Status {r.status_code}")
-        except Exception as e:
-            print("Error:", e)
+    payload = {
+        "machine_id": MACHINE_ID,
+        "sensor_values": generate_sensor_data()
+    }
+
+    try:
+        r = requests.post(BACKEND_URL, json=payload, timeout=2)
+        print("Sent:", payload, "Status:", r.status_code)
+    except Exception as e:
+        print("Error:", e)
+
     time.sleep(2)
